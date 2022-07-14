a509f64e560139acd4b086f55c5a9812ebe5fc0b
hrugved <whrugved@gmail.com>
2022-07-14 05:50:57 +0530

    init app

---
+++ app/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dapp Lottery</title>
+        <meta name="description" content="powered by smart contracts" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+hjello </div>
+  )
+}

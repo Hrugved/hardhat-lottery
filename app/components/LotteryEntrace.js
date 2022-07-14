b7ce6b310f7425dc4ee4e5a4e72ad3f1e1aceaf1
hrugved <whrugved@gmail.com>
2022-07-14 23:22:41 +0530

    add tx notification

--- app/components/LotteryEntrace.js
+++ app/components/LotteryEntrace.js
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react"
 import { useMoralis, useWeb3Contract } from "react-moralis"
 import { abi, contractAddresses } from "../constants"
 import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
 export default function LotteryEntrace() {
+
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
   const chainId = parseInt(chainIdHex)
   const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
   const [entranceFee, setEntranceFee] = useState("0")
+  const dispatch = useNotification()
+
   const { runContractFunction: enterRaffle } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
@@ -14,12 +18,14 @@ export default function LotteryEntrace() {
     params: {},
     msgValue: entranceFee,
   })
+
   const { runContractFunction: getEntranceFee } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
     functionName: "getEntranceFee",
     params: {},
   })
+
   useEffect(() => {
     if (isWeb3Enabled) {
       async function update() {
@@ -29,11 +35,30 @@ export default function LotteryEntrace() {
       update()
     }
   }, [isWeb3Enabled])
+
+  const handleSuccess = async function (tx) {
+    await tx.wait(1)
+    handleNewNotification(tx)
+  }
+
+  const handleNewNotification = function () {
+    dispatch({
+      type: 'info',
+      message: 'transaction complete',
+      title: 'Tx notification',
+      position: 'topR',
+      icon: 'bell'
+    })
+  }
+
   return (
     <div>
       {raffleAddress ? (
         <div>
-          <button onClick={async () => await enterRaffle()}>Enter Raffle</button>Entrance Fee {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+          <button onClick={async () => await enterRaffle({
+            onSuccess: handleSuccess,
+            onError: e => console.log(e)
+          })}>Enter Raffle</button>Entrance Fee {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
       ) : (
         <div>no Raffle address detected</div>
       )} 
